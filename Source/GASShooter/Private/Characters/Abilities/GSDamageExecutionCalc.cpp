9008e924c1f926c6d787595914abbf481c08af09
ibrahim-syah <ibrahim.syah.q@gmail.com>
2024-08-05 08:19:23 +0700

    wtf now it works?

--- Source/GASShooter/Private/Characters/Abilities/GSDamageExecutionCalc.cpp
+++ Source/GASShooter/Private/Characters/Abilities/GSDamageExecutionCalc.cpp
@@ -73,7 +73,9 @@ void UGSDamageExecutionCalc::Execute_Implementation(const FGameplayEffectCustomE
 
 	// Check for headshot. There's only one character mesh here, but you could have a function on your Character class to return the head bone name
 	const FHitResult* Hit = Spec.GetContext().GetHitResult();
-	if (AssetTags.HasTagExact(FGameplayTag::RequestGameplayTag(FName("Effect.Damage.CanHeadShot"))) && Hit && Hit->BoneName == "b_head" || Hit->BoneName == "head")
+	const FGameplayTag canHeadShotTag = FGameplayTag::RequestGameplayTag(FName("Effect.Damage.CanHeadShot"));
+	const bool hasTag = AssetTags.HasTagExact(canHeadShotTag);
+	if (hasTag && Hit != NULL && Hit->BoneName == "head")
 	{
 		UnmitigatedDamage *= HeadShotMultiplier;
 		FGameplayEffectSpec* MutableSpec = ExecutionParams.GetOwningSpecForPreExecuteMod();
